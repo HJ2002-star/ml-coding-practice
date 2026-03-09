95baf31bca6fa93da2513d12fc0d72f16bd4d895
박덕현 <park20020317@gmail.com>
2026-03-09 14:38:50 +0900

    Updated Changes

--- week01/python_basic(2021243021 박덕현).py
+++ week01/python_basic(2021243021 박덕현).py
@@ -111,3 +111,27 @@ print(a * 3)
 # 리스트 수정
 a[2] = 99
 print(a)
+
+a[1:2] = ['a', 'b', 'c']
+print(a)
+
+a[-1] = ['d', 'e', 'f']
+print(a)
+
+# 삭제 
+del a[-1]
+print(a)
+
+# 원소 추가
+a.append(5)
+print(a)
+
+# 원소 정렬
+b.sort()
+print(a)
+
+# 원소 순서 뒤집기
+a = [3, 4, 1, 9]
+a.reverse()
+print(a)
+
