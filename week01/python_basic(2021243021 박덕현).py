49e79edd6a9c80f0594936c5f4bcb25226fd198a
박덕현 <park20020317@gmail.com>
2026-03-09 15:12:50 +0900

    Updated Changes

--- week01/python_basic(2021243021 박덕현).py
+++ week01/python_basic(2021243021 박덕현).py
@@ -404,5 +404,16 @@ print(sorted([3, 0, 2, 1]))
 print(sorted('Python'))
 
 # 패키지, 모듈 사용
+# Request('http://www.sunmoon.ac.kr') # 오류 발생
 
+import urllib.request
+urllib.request.Request('http://www.sunmoon.ac.kr')
 
+import pandas
+pandas.DataFrame()
+
+from datetime import datetime
+datetime.now()
+
+# 파일 객체 생성
+f = open()
