253be0a03e2d0a332bc808a28b3e95738c80aeff
박덕현 <park20020317@gmail.com>
2026-03-09 15:10:50 +0900

    Updated Changes

--- week01/python_basic(2021243021 박덕현).py
+++ week01/python_basic(2021243021 박덕현).py
@@ -391,4 +391,18 @@ print(c)
 # 입력 받은 숫자에 해당되는 버위의 값을 반환
 print(range(5))
 print(list(range(5)))
+print(list(range(5,10)))
+print(list(range(5,10,2)))
+
+for i in range(5, 20, 3):
+    print(i)
+
+# 입력값 s의 길이를 반환
+len('Python')
+
+print(sorted([3, 0, 2, 1]))
+print(sorted('Python'))
+
+# 패키지, 모듈 사용
+
 
