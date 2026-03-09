b7e81e8a779bdd9842735f4219603a6636dbdee2
박덕현 <park20020317@gmail.com>
2026-03-09 15:17:20 +0900

    Updated Changes

--- week01/python_basic(2021243021 박덕현).py
+++ week01/python_basic(2021243021 박덕현).py
@@ -456,3 +456,12 @@ for line in lines:
     print(line)
 f.close()
 
+# 파일 모드 'r' -read()
+f= open('example.txt','r')
+data = f.read()
+f.close()
+data
+
+# with open() as 파일 객체
+with open('example.txt', 'w') as f:
+f.write()
