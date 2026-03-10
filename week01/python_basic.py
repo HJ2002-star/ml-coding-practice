686755b59d436c2609e6e2467fef27dbb4aa217f
박덕현 <park20020317@gmail.com>
2026-03-10 15:58:44 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -289,5 +289,14 @@ test_list = ['one', 'two', 'three']
 for i in test_list:
     x = i + '!'
     print(x)
-    
+
+# for 반복문2
+number = 0
+for score in [90, 25, 67, 45, 93]:
+    number += 1
+
+    if score > 60:
+        print("%d번 학생은 합격입니다." %number)
+    else:
+        print("%d번 학생은 불합격입니다." %number)
     
