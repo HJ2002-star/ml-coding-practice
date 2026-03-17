c74f981841cfd4b39b6e4f179e4919622c21d014
박덕현 <park20020317@gmail.com>
2026-03-17 15:55:47 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -58,5 +58,5 @@ print(less2015.head())
 
 print((netflix['release_year'] > 2015) & (netflix['type'] == 'TVShow'))
 
-more2015_tv = netflix[~(netflix['release_year'] > 2015))]
+more2015_tv = netflix[(netflix['release_year'] > 2015) &] 
 print()
