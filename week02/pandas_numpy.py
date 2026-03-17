19bd38270e299b099407a7d3d605bc59bcb77035
박덕현 <park20020317@gmail.com>
2026-03-17 14:15:28 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -23,4 +23,6 @@ file_url = 'https://media.githubusercontent.com/media/musthave-ML10/data_source/
 sample_df = pd.read_csv(file_url, index_col=0)
 print(sample_df.head())
 
-print(sampl)
+print(sample_df['var_5'])
+
+print(sample_df['var_1', 'var_2']) # [ ] 안에는 하나의 값만 들어갈 수 있음.
