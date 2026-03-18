add725ae16b3629be96d6e399a86ddc82378e341
박덕현 <park20020317@gmail.com>
2026-03-18 13:23:35 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -181,6 +181,7 @@ print(pd.DataFrame(sample_np, columns= sample_df.columns)) # 새 프레임 워�
 print(sample_np)
 print(sample_np[0])
 print(sample_np[0,2])
+print(sample_np[0:3,2:4])
 print(sample_np[:,2])
 
 np_a = np.array([[1,3],[0,-2]])
