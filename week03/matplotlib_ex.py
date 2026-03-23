3031f1b0156c663fb52ab4023416fa7307d04426
박덕현 <park20020317@gmail.com>
2026-03-23 10:43:09 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -23,5 +23,6 @@ pclass_survived_mean
 import matplotlib.pyplot as plt
 
 # 선 그래프 그리기
-plt.plot(pclass_survived_mean['Pclass'], pclass_survived_mean['Survived']),
-        marker = 'o', linestyle '-',
+plt.plot(pclass_survived_mean['Pclass'], pclass_survived_mean['Survived'],
+        marker = 'o', linestyle = '-', color = 'violet')
+plt.title('Survival Rate Variation Across Passenger')
