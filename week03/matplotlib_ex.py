d605d18ab83ee5ad147bcbdd7600a4907b1559b5
박덕현 <park20020317@gmail.com>
2026-03-24 13:00:52 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -332,5 +332,11 @@ plt.close()
 parch_counts = titanic.groupby('Parch')['Survived'].value_counts().unstack().fillna(0)
 print(parch_counts)
 
+# x,y 값 설정
+x = parch_counts.index.astype(str) #데이터프레임의 인덱스를 문자열로 변환
+y1 = parch_counts[0].values # 사망자
+y2 = parch_counts[1].values # 생존자
 
+#plt.subplots() 함수를 사용하여 하나의 그림인 figure에 개별 서브플롯을 동시에 추가
+fig
 
