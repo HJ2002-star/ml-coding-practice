1f296cd50f5b8d55fd59e30268e0979412854724
박덕현 <park20020317@gmail.com>
2026-03-24 12:30:28 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -293,4 +293,4 @@ plt.figure(figsize=(10,10))
 #첫 번째 서브 플롯 설정(선 그래프)
 plt.subplot(2,1,1)
 #선그래프 그리기
-plt.plot(x,y1,'-o',color = 'indigo', marker)
+plt.plot(x,y1,'-o',color = 'indigo', markersize = 7,linewidth = 3, alpha = 0.7)
