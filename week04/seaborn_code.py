3ca3e338b6b37c1170d56558643abe70111e4067
박덕현 <park20020317@gmail.com>
2026-03-30 13:40:52 +0900

    Updated Changes

--- week04/seaborn_code.py
+++ week04/seaborn_code.py
@@ -3,9 +3,6 @@
 # 사본 라이브러리 불러오기
 import seaborn as sns
 
-import os  
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
-
 # **팁(tips) 데이터셋 불러오기**
 tips = sns.load_dataset('tips')
 print(tips.head())
