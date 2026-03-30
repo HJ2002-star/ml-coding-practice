8dcc5727365620bc7bfc1203e6191f51e6624784
박덕현 <park20020317@gmail.com>
2026-03-30 14:33:05 +0900

    Updated Changes

--- week04/data_preprocessing.py
+++ week04/data_preprocessing.py
@@ -72,4 +72,17 @@ housing_tr.loc[null_rows_idx].head()
 from sklearn.ensemble import IsolationForest
 
 isolation_forest = IsolationForest(random_state=42)
-outlier_pred = is
+outlier_pred = isolation_forest.fit_predict(X)
+
+outlier_pred
+
+housing = housing.iloc[outlier_pred == 1]
+housing_labels = housing_labels.iloc[outlier_pred == 1]
+
+housing_cat = housing[["ocean_proximity"]]
+housing_labels = housing_labels.iloc[outlier_pred == 1]
+
+# 텍스트와 범주형 특성 다루기
+housing_cat = housing[["ocean_proximiny"]]
+housing_cat.head(8)
+
