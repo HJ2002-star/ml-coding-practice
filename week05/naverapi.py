d1080fc71cfb49d477099c804899f024d83fdc34
박덕현 <park20020317@gmail.com>
2026-04-14 12:46:13 +0900

    Updated Changes

--- week05/naverapi.py
+++ week05/naverapi.py
@@ -3,3 +3,15 @@ import urllib.request
 import datetime
 import json
 
+client_id = '1muCmJeI4Ctd8uQEVZeo'
+client_secret = 'qUut4pYmqh'
+
+def main():
+
+    node = 'news'
+    srcText = input('검색어를 입력하세요: ')
+
+    cnt = 0
+    jsonResult = []
+
+    jsonResponse = getNaverSearch()
