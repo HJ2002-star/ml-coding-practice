a7834edf87bc10470042dd73cd246fad6f8beb44
박덕현 <park20020317@gmail.com>
2026-04-14 13:52:36 +0900

    Updated Changes

--- week05/tourism.py
+++ week05/tourism.py
@@ -37,7 +37,8 @@ def getTourusmStatsService(nat_cd, ed_cd, nStartYear, nEndYear):
             jsonData = getTourusmStatsItem(yyyymm, nat_cd, ed_cd)   #[CODE 2]
             if (jsonData['reponse']['header']['resultMsg'] == 'OK'):
                 #데이터가 없는 마지막 항목인 경우 ----------------------------
-                if jsonData['reponse']['body']['item']
+                if jsonData['reponse']['body']['items'] == '':
+                    dataEND = "{0}{1:0>2}".format(str())
 
 """ [CODE 2]"""
 
