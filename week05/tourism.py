8d3091e024203921b237206e4ea1316331520065
박덕현 <park20020317@gmail.com>
2026-04-14 14:39:04 +0900

    Updated Changes

--- week05/tourism.py
+++ week05/tourism.py
@@ -15,10 +15,10 @@ def main():
     print("<< 국내 입국한 외국인의 통계 데이터를 수집합니다. >>")
     nat_cd = input('국가 코드를 입력하세요(중국: 112 / 일본: 130 / 미국: 275) :')
     nStartYear = int(input('데이터를 몇 년부터 수집할까요? : '))
-    eEndYear = int(input('데이터를 몇 년까지 수집할까요? : '))
+    nEndYear = int(input('데이터를 몇 년까지 수집할까요? : '))
     ed_cd = "E" #E: 방한외래관광객, D: 해외 출국
 
-    jsonResult, result, natName, dataEND = getTourusmStatsService(nat_cd, ed_cd, nStartYear, eEndYear) #[CODE 3]
+    jsonResult, result, natName, dataEND = getTourusmStatsService(nat_cd, ed_cd, nStartYear, nEndYear) #[CODE 3]
 
     #파일저장 :  csv 파일
     columns = ["입국자국가", "국가코드", "입국연월", "입국자 수"]
