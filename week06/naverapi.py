658f37627806bf7a12fa3e8febc62d13ea3a367b
박덕현 <park20020317@gmail.com>
2026-04-20 13:49:13 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -38,7 +38,9 @@ def main():
 
 
 def getNaverSearch(node, srcText, page_start, display):
-
+    base = "https://openapi.naver.com/v1/search"
+    node = "/%s.json" % node
+    parameters = "?query=%s&start=%s&display=%s" % ()
 def getRequestUrl(url):
     req = urllib.request.Request(url)
 
