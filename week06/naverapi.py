1232693f824740b556aeba907c2c14f807d5ba32
박덕현 <park20020317@gmail.com>
2026-04-20 13:55:13 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -64,5 +64,8 @@ def getRequestUrl(url):
             return response.read().decode('utf-8')
     except Exception as e:
         print(e)
-        print
+        print("[%s] Error for URL : %s" % (datetime.datetime.now(), url))
+        return None
+    
+    
 def getPostData(post, jsonResult, cnt): #[CODE 3]
