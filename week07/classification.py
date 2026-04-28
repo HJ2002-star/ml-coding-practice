00f980dc69eb20f88d27aee2ed0581f0f8ef3c15
박덕현 <park20020317@gmail.com>
2026-04-28 16:08:12 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -40,3 +40,13 @@ y_train_5 = (y_train == '5')    # 5는 True고, 다른 숫자는 모두 False
 y_test_5 = (y_test == '5')
 
 from sklearn.linear_model import SGDClassifier
+
+sgd_clf = SGDClassifier(random_state=42)
+sgd_clf.fit(X_train, y_train_5)
+
+sgd_clf.predict([some_digit])
+
+# 성능 측정 - 교차 검증을 사용한 정확도 측정
+from sklearn.model_selection import cross_val_score
+
+cross_val_score(sgd_clf, X_train, y_train, cv=3, scoring="accuracy")
