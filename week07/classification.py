b038b0265817dbef0ed9f451f518de17a15534cf
박덕현 <park20020317@gmail.com>
2026-04-28 15:59:42 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -10,3 +10,9 @@ print(X)
 print(X.shape)      # 28 x 28개의 픽셀 특징을 가진 이미지 70,000개
 print(y)
 print(y.shape)
+
+import matplotlib.pyplot as plt
+
+def plot_digit(image_data):
+    image = image_data.reshape(28,28)
+    plt.imshow(image, cmap="binary")
